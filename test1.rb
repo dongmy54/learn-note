e3848ada3077425506b066b8dc04c07d0fd064a4
dongmy54 <615760433@qq.com>
2018-04-10 18:37:10 +0800

    update 2018.4.10

--- test1.rb
+++ test1.rb
@@ -1,16 +1,27 @@
-# GET 请求 带cookie
+# 获取 响应信息
 require 'net/http'
-require 'json'
 
-uri = URI('http://hhhhh.t.yurl.vip/api/v1/account/favorite')
-params = { :id_type => 2 }
-uri.query = URI.encode_www_form(params)
-# 初始化请求
-req = Net::HTTP::Get.new(uri)
-# 设置请求cookie
-req['Cookie'] = '_homeland_shop_customer_session=e0ab8ec558d6b8fbfc00880f08a8dd1c513ac3ec307ef90bdc76426053389e27'
-# 响应
-res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-  http.request(req)
-end
-puts JSON.parse(res.body)
+uri = URI('http://baidu.com')
+res = Net::HTTP.get_response(uri)
+
+# 获取响应所有头部
+puts res.each_header.to_h
+# => {"date"=>"Tue, 10 Apr 2018 10:31:25 GMT", "server"=>"Apache", "last-modified"=>"Tue, 12 Jan 2010 13:48:00 GMT", "etag"=>"\"51-47cf7e6ee8400\"", "accept-ranges"=>"bytes", "content-length"=>"81", "cache-control"=>"max-age=86400", "expires"=>"Wed, 11 Apr 2018 10:31:25 GMT", "connection"=>"Keep-Alive", "content-type"=>"text/html"}
+
+#获取单个头部 
+puts res['last-modified']
+# => Tue, 12 Jan 2010 13:48:00 GMT
+
+#获取状态码
+puts res.code
+# => 200
+
+# 获取响应信息
+puts res.message
+# => OK
+
+# 获取响应body
+puts res.body
+# => <html> 
+# <meta http-equiv="refresh" content="0;url=http://www.baidu.com/">
+# </html>
