22735581af41e37f7d19ea565d42aada97a3888c
dongmy54 <615760433@qq.com>
2018-04-11 11:48:19 +0800

    http 2018.4.11

--- ruby/http.rb
+++ ruby/http.rb
@@ -11,42 +11,6 @@ puts res.body
 
 
 #===================================================================================#
-# 常规 POST 请求 
-require 'net/http'
-require 'json'
-
-# 获取uri 对象
-uri = URI('http://account.test.yurl.vip/interface/account/login')
-# post_form(参数 字符串/符号 都行) 请求
-res = Net::HTTP.post_form(uri, {:loginid => '18200375xxx',:password => 'dmyxxx'})
-# 解析成hash
-puts JSON.parse(res.body)
-# => {"data"=>{"loginid"=>"18200375xxx", "is_partner"=>false, "is_agent"=>false}}
-
-
-#===================================================================================#
-# GET 请求 带cookie
-require 'net/http'
-require 'json'
-
-uri = URI('http://hhhhh.t.yurl.vip/api/v1/account/favorite')
-params = { :id_type => 2 }
-uri.query = URI.encode_www_form(params)
-# 初始化请求
-req = Net::HTTP::Get.new(uri)
-# 设置请求cookie
-req['Cookie'] = '_homeland_shop_customer_session=e0ab8ec558d6b8fbfc00880f08a8dd1c513ac3ec307ef90bdc76426053389e27'
-# 响应
-res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-  http.request(req)
-end
-puts JSON.parse(res.body)
-# => {"code"=>200, "message"=>"", "favorites"=>[{"id"=>5, "favorite_id"=>4675, 
-# "id_type"=>2, "status"=>nil, "customer_id"=>16993, "add_time"=>"2018-03-19T15:30:20.132+08:00"}, 
-# {"id"=>6, "favorite_id"=>4671, "id_type"=>2, "status"=>nil, "customer_id"=>16993, "add_time"=>"2018-03-19T15:31:01.491+08:00"}]}
-
-
-#===================================================================================#
 # 获取 响应信息
 require 'net/http'
 
@@ -74,3 +38,127 @@ puts res.body
 # => <html> 
 # <meta http-equiv="refresh" content="0;url=http://www.baidu.com/">
 # </html>
+
+
+#===================================================================================#
+# 常规 POST 请求 
+require 'net/http'
+require 'json'
+
+# 获取uri 对象
+uri = URI('http://account.test.yurl.vip/interface/account/login')
+# post_form(参数 字符串/符号 都行) 请求
+res = Net::HTTP.post_form(uri, {:loginid => '18200375xxx',:password => 'dmyxxx'})
+# 解析成hash
+puts JSON.parse(res.body)
+# => {"data"=>{"loginid"=>"18200375xxx", "is_partner"=>false, "is_agent"=>false}}
+
+
+#===================================================================================#
+# GET 请求 带cookie
+require 'net/http'
+require 'json'
+
+uri = URI('http://hhhhh.t.yurl.vip/api/v1/account/favorite')
+params = { :id_type => 2 }
+uri.query = URI.encode_www_form(params)
+# 初始化请求
+req = Net::HTTP::Get.new(uri)
+# 设置请求cookie
+req['Cookie'] = '_homeland_shop_customer_session=xxxxx'
+# 响应
+res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+  http.request(req)
+end
+puts JSON.parse(res.body)
+# => {"code"=>200, "message"=>"", "favorites"=>[{"id"=>5, "favorite_id"=>4675, 
+# "id_type"=>2, "status"=>nil, "customer_id"=>16993, "add_time"=>"2018-03-19T15:30:20.132+08:00"}, 
+# {"id"=>6, "favorite_id"=>4671, "id_type"=>2, "status"=>nil, "customer_id"=>16993, "add_time"=>"2018-03-19T15:31:01.491+08:00"}]}
+
+
+#===================================================================================#
+# POST 带 cookie 带 表单参数
+require 'net/http'
+
+uri = URI('http://hhhhh.t.yurl.vip/api/v1/account/favorite/create')
+# POST GET 都是 Net::HTTP模块下的类
+req = Net::HTTP::Post.new(uri)
+# set_from_data 直接在req 上调用
+req.set_form_data({:favorite_id => 2, :type_id => 2})
+req['Cookie'] = '_homeland_shop_customer_session=xxxxxx'
+# Net::HTTP.start 通用
+res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+  http.request(req)
+end
+
+puts res.body
+# => {"code":201,"message":"收藏对象不存在"}
+
+
+#===================================================================================#
+# 串起来
+# 先：一个请求获取token码
+# 后：带着token 发下一个请求
+require 'net/http'
+require 'json'
+# 先获取token码
+uri_pre = URI('http://hhhhh.t.yurl.vip/api/v1/account/login')
+# post_form 快捷方式 post 数据
+res_pre = Net::HTTP.post_form(uri_pre, {:account => '1820037xxxx', :password => 'dmxxxx'})
+token = JSON.parse(res_pre.body)["token"]
+
+uri = URI('http://hhhhh.t.yurl.vip/api/v1/account/favorite/create')
+# POST GET 都是 Net::HTTP模块下的类
+req = Net::HTTP::Post.new(uri)
+# set_from_data 直接在req 上调用
+req.set_form_data({:favorite_id => 2, :type_id => 2})
+req['Cookie'] = "_homeland_shop_customer_session=#{token}"
+# Net::HTTP.start 通用
+res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+  http.request(req)
+end
+
+puts res.body
+# => {"code":201,"message":"收藏对象不存在"}
+
+
+#===================================================================================#
+# Net::HTTP.start 万能的
+# 不同的请求 指定不同的类 
+# Net::HTTP::Get / Post / Put ...
+require 'net/http'
+
+uri = URI('http://api.test.yurl.vip/v1/product_types/850')
+req = Net::HTTP::Put.new(uri)
+# 指定类型
+req.content_type = 'application/json'
+req.body = '{
+  "product_types":{
+    "name":"厨具",
+    "handle":"sqwee"
+  }
+}'
+req['X-API-ACCESS-TOKEN'] = 'd1748cf1a1e94c38xxxxx'
+
+res = Net::HTTP.start(uri.hostname,uri.port) do |http|
+  http.request(req)
+end
+
+puts res.body
+# => {"code":422,"errors":{"common":["product_type不存在"]}}
+
+
+#===================================================================================#
+# 基本认证 （basic_auth)
+require 'net/http'
+
+uri = URI('http://www.test.yurl.vip/')
+req = Net::HTTP::Get.new(uri)
+req.basic_auth('xxxxx','xxxxx')
+
+res = Net::HTTP.start(uri.hostname,uri.port) do |http|
+  http.request(req)
+end
+
+puts res.body
+# => 返回页面html ....
