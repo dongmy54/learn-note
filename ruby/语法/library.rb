9c3285489fa16593e438aca0f7db54d527796b10
dongmy54 <615760433@qq.com>
2018-06-19 11:31:46 +0800

    update

--- ruby/语法/library.rb
+++ ruby/语法/library.rb
@@ -83,6 +83,8 @@ DateTime.parse("#{end_at}T23:59:59+08:00")   # 添加 时分秒 成时间戳
 # => 2018-06-18T23:59:59+08:00
 DateTime.parse("#{end_at}T23:59:59+08:00").to_time # 转时间
 # => 2018-06-18 23:59:59 +0800
-puts DateTime.parse("#{end_at}T23:59:59+08:00").to_time.utc # 转utc时间
+DateTime.parse("#{end_at}T23:59:59+08:00").to_time.utc # 转utc时间
 # => 2018-06-18 15:59:59 UTC
+DateTime.parse("#{end_at}T23:59:59+08:00").to_time.utc.localtime('+08:00') # 转本地时间 加时区
+# => 2018-06-18 23:59:59 +0800
 
