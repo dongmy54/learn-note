7cc7e3fff4868402a5fc891086094134ccfc0228
dongmy54 <615760433@qq.com>
2018-07-13 18:23:51 +0800

    active support

---
+++ ruby/语法/active_support.rb
@@ -0,0 +1,21 @@
+require 'active_support'
+require 'active_support/core_ext/object'
+require 'active_support/core_ext/string'
+require 'active_support/core_ext/array'
+require 'active_support/core_ext/enumerable'
+
+# 数组中 不包含
+%w(sda sdaf sdaf).exclude?('qw') 
+# => true
+
+# 字符串中 不包含
+"sdaf".exclude?('sd')
+# => false
+
+# 字符串转常量
+'Module'.constantize
+# Module 常量
+
+#驼峰命令
+'active_record'.camelize
+# => "ActiveRecord"
