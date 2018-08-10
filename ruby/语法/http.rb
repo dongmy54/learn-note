932e0b3bbde31568ecd4fdeecd4302f2403cfd61
dongmy54 <615760433@qq.com>
2018-08-10 18:09:33 +0800

    2018.8.10 http

--- ruby/语法/http.rb
+++ ruby/语法/http.rb
@@ -1,164 +1,120 @@
-# GET 带查询参数 get_response
-require 'net/http'
+require 'net/http'    # 会自动 require 'uri'
+require 'json'
 
-uri = URI('http://hhhhh.t.yurl.vip/api/v1/account/favorite')
-params = { :id_type => 2 }
-uri.query = URI.encode_www_form(params)
-
-res = Net::HTTP.get_response(uri)
-puts res.body
-# => {"code":212,"message":"账号未登录，无操作权限"}
+#===================================================================================#
+# 响应 信息
+res.msg    # 等价于 message
+# => ok
+res.code 
+# => 200
+res.body   # JSON 格式 
+# => {"code":201,"msg":{"desc":"推荐导航已经存在"}}
+res.uri  
+# http://3232qw.t.yurl.vip/center/navigation/create_recommen
+res.to_hash
+# => {"date"=>["Fri, 10 Aug 2018 09:13:14 GMT"], "content-type"=>["application/json;charset=utf-8"], 
+# "content-length"=>["54"], "connection"=>["keep-alive"], "cache-control"=>["no-store"], 
+# "pragma"=>["no-cache"], "x-content-type-options"=>["nosniff"], 
+# "set-cookie"=>["_homeland_shop_admin_session=39a44acc745b6b80a8c73c8781944bbffddbcdb33b831d48fba32f9f28807c06; path=/; 
+# expires=Fri, 10 Aug 2018 11:13:14 -0000; HttpOnly"], "server"=>["XiaoBaWang"]} 
 
 
 #===================================================================================#
-# 获取 响应信息
-require 'net/http'
+# GET get_response
+uri = URI("https://www.baidu.com/")
 
-uri = URI('http://baidu.com')
 res = Net::HTTP.get_response(uri)
-
-# 获取响应所有头部
-puts res.each_header.to_h
-# => {"date"=>"Tue, 10 Apr 2018 10:31:25 GMT", "server"=>"Apache", "last-modified"=>"Tue, 12 Jan 2010 13:48:00 GMT", "etag"=>"\"51-47cf7e6ee8400\"", "accept-ranges"=>"bytes", "content-length"=>"81", "cache-control"=>"max-age=86400", "expires"=>"Wed, 11 Apr 2018 10:31:25 GMT", "connection"=>"Keep-Alive", "content-type"=>"text/html"}
-
-#获取单个头部 
-puts res['last-modified']
-# => Tue, 12 Jan 2010 13:48:00 GMT
-
-#获取状态码
-puts res.code
-# => 200
-
-# 获取响应信息
-puts res.message
-# => OK
-
-# 获取响应body
 puts res.body
-# => <html> 
-# <meta http-equiv="refresh" content="0;url=http://www.baidu.com/">
+# <html>
+# <head>
+#   <script>
+#     location.replace(location.href.replace("https://","http://"));
+#   </script>
+# </head>
+# <body>
+#   <noscript><meta http-equiv="refresh" content="0;url=http://www.baidu.com/"></noscript>
+# </body>
 # </html>
 
 
 #===================================================================================#
-# 常规 POST 请求 
-require 'net/http'
-require 'json'
+# POST post_form 表单数据
+uri = URI("http://account.test.yurl.vip/interface/account/login")
 
-# 获取uri 对象
+res = Net::HTTP.post_form(uri,:loginid => '182003xxxx6',:password => 'yyyy')
+
+puts res.body
+# {"data":{"loginid":"182003xxxx6","is_partner":false,"is_agent":true}}
+
+
+#===================================================================================#
+# Net::HTTP.start 万用型
+uri = URI("http://3232qw.t.yurl.vip/center/navigation/create_recommen")
+
+res = Net::HTTP.start(uri.host,uri.port) do |http|
+  req          = Net::HTTP::Post.new(uri)        # 选择请求 类型 new 
+  req['token'] = 'token xxxxx yyyyy' # 设置头
+  req.set_form_data(:type => 'm')                # 表单数据
+  # req.body = {:a => 'hh'}.to_json              # draw数据 应用中用 request.body.read 读取
+  # req.content_type = 'multipart/form-data'     # 数据类型
+  # req.basic_auth 'user', 'password'            # 基本认证
+  
+  http.request(req)
+end
+
+puts res.body
+# {"code":202,"msg":{"desc":"推荐导航类型无效"}}
+
+
+#===================================================================================#
+# 响应类型 处理
+def res_method(uri)
+  uri = URI(uri)
+  res = Net::HTTP.get_response(uri)
+  
+  case res
+  when Net::HTTPSuccess      # 所有 2xx
+    puts '成功响应'
+  when Net::HTTPRedirection  # 所有 3xx
+    puts "正在重定向到: #{res['location']}"
+  else
+    puts "sorry! 发生了错误"
+    puts "信息：#{res.value}"
+  end
+end
+
+res_method("https://www.baidu.com/")
+# 成功响应
+res_method("http://3232qw.t.yurl.vip/center/main/#/product")
+# 正在重定向到: http://account.test.yurl.vip/login?from=3232qw.t.yurl.vip
+
+
+#===================================================================================#
+# 综合示例
+# get 登录 取 account_token
 uri = URI('http://account.test.yurl.vip/interface/account/login')
-# post_form(参数 字符串/符号 都行) 请求
-res = Net::HTTP.post_form(uri, {:loginid => '18200375xxx',:password => 'dmyxxx'})
-# 解析成hash
-puts JSON.parse(res.body)
-# => {"data"=>{"loginid"=>"18200375xxx", "is_partner"=>false, "is_agent"=>false}}
+res = Net::HTTP.post_form(uri,'loginid' => '18200375876','password' => 'dmy067')
 
+account_set_cookie = res.to_hash['set-cookie'].join('')
+account_token      = account_set_cookie[/_homeland_shop_account_session=(.+?);/,1]
 
-#===================================================================================#
-# GET 请求 带cookie
-require 'net/http'
-require 'json'
+# get 后台 取 admin_token
+uri = URI("http://3232qw.t.yurl.vip/center/sessions/encrpt_create?token=#{account_token}")
+res = Net::HTTP.get_response(uri)
+admin_set_cookie = res.to_hash['set-cookie'].join('')
+admin_token      = admin_set_cookie[/_homeland_shop_admin_session=(.*?);/,1]
 
-uri = URI('http://hhhhh.t.yurl.vip/api/v1/account/favorite')
-params = { :id_type => 2 }
-uri.query = URI.encode_www_form(params)
-# 初始化请求
-req = Net::HTTP::Get.new(uri)
-# 设置请求cookie
-req['Cookie'] = '_homeland_shop_customer_session=xxxxx'
-# 响应
-res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-  http.request(req)
-end
-puts JSON.parse(res.body)
-# => {"code"=>200, "message"=>"", "favorites"=>[{"id"=>5, "favorite_id"=>4675, 
-# "id_type"=>2, "status"=>nil, "customer_id"=>16993, "add_time"=>"2018-03-19T15:30:20.132+08:00"}, 
-# {"id"=>6, "favorite_id"=>4671, "id_type"=>2, "status"=>nil, "customer_id"=>16993, "add_time"=>"2018-03-19T15:31:01.491+08:00"}]}
-
-
-#===================================================================================#
-# POST 带 cookie 带 表单参数
-require 'net/http'
-
-uri = URI('http://hhhhh.t.yurl.vip/api/v1/account/favorite/create')
-# POST GET 都是 Net::HTTP模块下的类
-req = Net::HTTP::Post.new(uri)
-# set_from_data 直接在req 上调用
-req.set_form_data({:favorite_id => 2, :type_id => 2})
-req['Cookie'] = '_homeland_shop_customer_session=xxxxxx'
-# Net::HTTP.start 通用
-res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-  http.request(req)
-end
-
-puts res.body
-# => {"code":201,"message":"收藏对象不存在"}
-
-
-#===================================================================================#
-# 串起来
-# 先：一个请求获取token码
-# 后：带着token 发下一个请求
-require 'net/http'
-require 'json'
-# 先获取token码
-uri_pre = URI('http://hhhhh.t.yurl.vip/api/v1/account/login')
-# post_form 快捷方式 post 数据
-res_pre = Net::HTTP.post_form(uri_pre, {:account => '1820037xxxx', :password => 'dmxxxx'})
-token = JSON.parse(res_pre.body)["token"]
-
-uri = URI('http://hhhhh.t.yurl.vip/api/v1/account/favorite/create')
-# POST GET 都是 Net::HTTP模块下的类
-req = Net::HTTP::Post.new(uri)
-# set_from_data 直接在req 上调用
-req.set_form_data({:favorite_id => 2, :type_id => 2})
-req['Cookie'] = "_homeland_shop_customer_session=#{token}"
-# Net::HTTP.start 通用
-res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-  http.request(req)
-end
-
-puts res.body
-# => {"code":201,"message":"收藏对象不存在"}
-
-
-#===================================================================================#
-# Net::HTTP.start 万能的
-# 不同的请求 指定不同的类 
-# Net::HTTP::Get / Post / Put ...
-require 'net/http'
-
-uri = URI('http://api.test.yurl.vip/v1/product_types/850')
-req = Net::HTTP::Put.new(uri)
-# 指定类型
-req.content_type = 'application/json'
-req.body = '{
-  "product_types":{
-    "name":"厨具",
-    "handle":"sqwee"
-  }
-}'
-req['X-API-ACCESS-TOKEN'] = 'd1748cf1a1e94c38xxxxx'
+# post 接口
+uri = URI('http://3232qw.t.yurl.vip/center/navigation/create_recommen')
 
 res = Net::HTTP.start(uri.hostname,uri.port) do |http|
+  req          = Net::HTTP::Post.new(uri)
+  req['token'] = admin_token
+  req.set_form_data(:type => 'display')
+
   http.request(req)
 end
 
 puts res.body
-# => {"code":422,"errors":{"common":["product_type不存在"]}}
 
 
-#===================================================================================#
-# 基本认证 （basic_auth)
-require 'net/http'
-
-uri = URI('http://www.test.yurl.vip/')
-req = Net::HTTP::Get.new(uri)
-req.basic_auth('xxxxx','xxxxx')
-
-res = Net::HTTP.start(uri.hostname,uri.port) do |http|
-  http.request(req)
-end
-
-puts res.body
-# => 返回页面html ....
