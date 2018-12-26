e44e3392775ea8374b88ff01504166d7705309a9
dongmy54 <615760433@qq.com>
2018-12-26 19:21:26 +0800

    2018.12.26 update

--- ruby/语法/temp.rb
+++ ruby/语法/temp.rb
@@ -11,3 +11,33 @@ puts a.tainted?  # => true
 a.untaint
 
 puts a.tainted?  # => false
+
+
+# dup 不能复制单例方法 #
+a = Object.new
+
+def a.foo
+  puts 'foo'
+end
+
+b = a.clone.foo
+#c = b.dup.foo
+# undefined method `foo' for nil:NilClass (NoMethodError)
+
+
+#===================================dup Vs clone ========================================#
+# clone 会保持原有的状态（冻结）
+class Post
+  attr_accessor :title
+end
+
+p = Post.new.freeze
+
+p1 = p.dup
+p1.title = 'hello'
+
+p2 = p.clone
+p2.title = 'hello'
+# can't modify frozen Post (FrozenError)
+
+
