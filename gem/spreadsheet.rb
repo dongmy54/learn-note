1f02008e3df8b8875f7c0c353f8bdf7eb27fd497
dongmy54 <615760433@qq.com>
2019-04-08 16:39:41 +0800

    update spreadsheet gem

--- gem/spreadsheet.rb
+++ gem/spreadsheet.rb
@@ -6,20 +6,23 @@ Spreadsheet.client_encoding = 'UTF-8'
 book = Spreadsheet::Workbook.new
 sheet1 = book.create_worksheet name: '测试spreadsheet'
 
+# 首行标题
+sheet1[0,0] = '我是合并单元格,合并了一行三列'
+sheet1.merge_cells(0,0,0,2) # 开始行 开始列 结束行 结束列
 # 写数据
-sheet1.row(0).concat %w(名字 国家 学历)
+sheet1.row(1).concat %w(名字 国家 学历)
 
-sheet1.row(1).concat %w(dmy china bk)
-sheet1[2,0] = 'zhanglong'
-sheet1[2,1] = 'japan'
-sheet1[2,2] = 'bk'
+sheet1.row(2).concat %w(dmy china bk)
+sheet1[3,0] = 'zhanglong'
+sheet1[3,1] = 'japan'
+sheet1[3,2] = 'bk'
 
-row = sheet1.row(3)
+row = sheet1.row(4)
 row.push 'huawugui'
 row.push 'china'
 row.push 'bk'
 
-sheet1.row(4).push 'hh', 'armerica', 'ss'
+sheet1.row(5).push 'hh', 'armerica', 'ss'
 
 # 样式
 sheet1.row(0).height = 18
@@ -29,5 +32,3 @@ format = Spreadsheet::Format.new :color => :blue,
 sheet1.row(0).default_format = format
 
 book.write '/Users/dmy/Downloads/test.xls'
-
-
