a5492696ca587c69daf8d1e0b04841f2006d0840
dongmingyan <dongmingyan01@gmail.com>
2019-07-19 15:15:48 +0800

    update ruby

--- ruby/语法/temp.rb
+++ ruby/语法/temp.rb
@@ -1,4 +1,53 @@
 #===================================================================================#
+# freeze 冻结不可变
+# 1. 字符串、数组、hash都可用
+# 2. 减少内存分配
+# 3. hash中字符串，自带冻结
+str = 'hello'.freeze
+str[0] = 'q'      # RuntimeError: can't modify frozen String
+str << 'sd'       # RuntimeError: can't modify frozen String
+
+arr = [1,3,9].freeze
+arr[0] = 'cdf'    # RuntimeError: can't modify frozen Array
+arr << 'we'       # RuntimeError: can't modify frozen Array
+
+hash = {a: 1,b: 2}.freeze
+hash[:a] = 'gy'   # RuntimeError: can't modify frozen Hash
+
+person = {'zhangsan' => {id: 1,age: 18}, 'lisi' => {id: 2, age: 24}}
+person.keys.first.frozen?  # => true
+
+
+
+
+#===================================================================================#
+# clone VS dup
+# 1. 都是浅层复制
+# 2. clone克隆的更彻底（冻结状态、单例方法）
+hash = {a: 1, b: 2}.freeze
+
+hash2 = hash.clone
+hash2[:a] = '3'      # RuntimeError: can't modify frozen Hash
+
+hash3 = hash.dup
+hash3[:a] = '3'      # => => "3"
+
+# 单例方法dup不能复制
+temp_obj = Object.new
+
+def temp_obj.foo
+  puts 'hello'
+end
+
+obj1 = temp_obj.clone
+obj1.foo            # hello
+
+obj2 = temp_obj.dup
+obj2.foo            # NoMethodError: undefined method `foo' for #<Object:0x007fa3dc044930>
+
+
+
+#===================================================================================#
 # 污染对象
 # ruby 会把从外界获取的值 自动标为污染对象
 # tainted? 是否为污染
@@ -13,31 +62,5 @@ a.untaint
 puts a.tainted?  # => false
 
 
-# dup 不能复制单例方法 #
-a = Object.new
-
-def a.foo
-  puts 'foo'
-end
-
-b = a.clone.foo
-#c = b.dup.foo
-# undefined method `foo' for nil:NilClass (NoMethodError)
-
-
-#===================================dup Vs clone ========================================#
-# clone 会保持原有的状态（冻结）
-class Post
-  attr_accessor :title
-end
-
-p = Post.new.freeze
-
-p1 = p.dup
-p1.title = 'hello'
-
-p2 = p.clone
-p2.title = 'hello'
-# can't modify frozen Post (FrozenError)
 
 
