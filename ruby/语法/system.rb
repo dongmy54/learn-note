decde4b4d7136ed6a583e167b49145aaa169c071
dongmingyan <dongmingyan01@gmail.com>
2019-09-06 11:37:00 +0800

    update ruby

---
+++ ruby/语法/system.rb
@@ -0,0 +1,28 @@
+##### system 
+# 作用：让ruby可以执行外部命令
+# 返回值 
+# true 成功执行
+# false 执行错误
+# nil 命令不存在
+
+
+system('ruby t.rb')
+# 1 打印内容
+# => true
+
+
+system('ls')
+# Gemfile   Gemfile.rb  Rakefile  bin   config.ru lib   public    tmp
+# Gemfile.lock  README.rdoc app   config    db    log   test    vendor
+# => true
+
+
+
+
+
+
+
+
+
+
+
