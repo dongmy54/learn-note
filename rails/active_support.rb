2b8cf4b23225b06947918c13764eef0210a9189e
dongmingyan <dongmingyan01@gmail.com>
2019-09-19 09:38:25 +0800

    update

--- rails/active_support.rb
+++ rails/active_support.rb
@@ -265,6 +265,19 @@ h
 # => {:c=>12}
 
 
+# 类方法 from_xml xml 转hash
+xml = <<-XML
+  <?xml version="1.0" encoding="UTF-8"?>
+    <hash>
+      <foo type="integer">1</foo>
+      <bar type="integer">2</bar>
+    </hash>
+XML
+
+hash = Hash.from_xml(xml)
+# => {"hash"=>{"foo"=>1, "bar"=>2}}
+
+
 
 
 
