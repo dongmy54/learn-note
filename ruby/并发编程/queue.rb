147fa41079559767032eb30565ccb988587b7cf7
dongmingyan <dongmingyan01@gmail.com>
2019-11-20 14:46:33 +0800

    add ruby queue

---
+++ ruby/并发编程/queue.rb
@@ -0,0 +1,28 @@
+queue = Queue.new
+
+puts queue.size   # 0
+puts queue.empty? # true
+puts queue.pop    # 空队列会报错
+# No live threads left. Deadlock? (fatal) 
+
+queue << 'first'
+queue << 'second'
+queue << 'third'
+puts queue.size     # 2
+puts queue.empty?   # false
+puts queue.pop
+# first
+puts queue.pop
+# second
+
+queue.clear
+puts queue.empty?  # true
+
+queue << 'sdaf'
+queue.close
+queue.closed?  # true
+
+puts queue.pop # close后仍可pop 
+# sdaf
+queue << 'saf' # close后不可追加
+# queue closed (ClosedQueueError)
