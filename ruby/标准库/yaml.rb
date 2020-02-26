0371c6cb9f7c62e296db2b1c20b82cb18798365b
dongmingyan <dongmingyan01@gmail.com>
2020-02-26 10:50:43 +0800

    update yaml

--- ruby/标准库/yaml.rb
+++ ruby/标准库/yaml.rb
@@ -1,4 +1,39 @@
+# ================== yaml 文件注意 =========================#
+# 不可直接使用部分替换，yaml中只能完整替换
+# 可用ruby的方式传入hash 进行替换
+# & 定义yaml变量 * 使用变量 << 注入
+# 读取出来键都是字符串
+
+```ruby
+# /Users/dongmingyan/test.yml
+
+hu: &hu 我是单行变量
+bar: *hu # 直接使用hu
+
+default_hash: &default_hash
+  first: 第一行
+  second: 第二行
+  three: 第三行
+
+hash:
+  <<: *default_hash  # 使用变量hash
+  first: 我要替换     # 覆盖前面的值
+
+ruby_inject_value: 'I like %{thing}'  # 必须引号
+```
+
 require 'yaml'
+
+# 加载文件
+file = YAML.load_file('/Users/dongmingyan/test.yml')
+# {"hu"=>"我是单行变量",
+# "bar"=>"我是单行变量",
+# "default_hash"=>{"first"=>"第一行", "second"=>"第二行", "three"=>"第三行"},
+# "hash"=>{"first"=>"我要替换", "second"=>"第二行", "three"=>"第三行"},
+# "ruby_inject_value"=>"I like %{thing}"}
+
+file['ruby_inject_value'] %{thing: 'sing'} # 注入hash
+# => "I like sing"
 
 # 解析yaml
 YAML.load("---\n:a: b\n")
@@ -8,15 +43,5 @@ YAML.load("---\n:a: b\n")
 YAML.dump({:a => 'b'})
 # => "---\n:a: b\n"
 
-# 加载文件
-YAML.load_file('/Users/dmy/test.yml')
-# {
-#   "dmy" => {
-#     "name" => "dmy",
-#     "age" => 26
-#     },
-#   "zhangsan" => {
-#       "name" => "zs",
-#       "age" => 23
-#   }
-# }
+
+
