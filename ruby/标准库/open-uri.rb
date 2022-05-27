852b918f3fd52fbe1d40a22d8545362dcb573d5f
dongmingyan <dongmingyan01@gmail.com>
2022-05-27 15:24:51 +0800

    update

--- ruby/标准库/open-uri.rb
+++ ruby/标准库/open-uri.rb
@@ -52,7 +52,7 @@ require 'open-uri'
 url      = "http://www.ruby-lang.org/"
 filename = "cathedral.html"
 File.open(filename, 'w') do |f|
-  # 直接 open read
+  # 直接 open read 读取的是文件内容 直接写入文件就行
   text = open(url).read
   # 写入 文件
   f.write text
