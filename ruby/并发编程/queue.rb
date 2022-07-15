b39ccb97dfccaa6ad2e9b650d4213bdf0d6c2996
dongmingyan <dongmingyan01@gmail.com>
2022-07-15 15:23:02 +0800

    update ruby queue

--- ruby/并发编程/queue.rb
+++ ruby/并发编程/queue.rb
@@ -1,3 +1,6 @@
+# 1. 它是线程安全的
+# 2. 如果我们直接join 一个线程内部（queue.pop)会报死锁，所以不能用于阻塞主线程的情况
+
 queue = Queue.new
 
 puts queue.size   # 0
@@ -26,3 +29,10 @@ puts queue.pop # close后仍可pop
 # sdaf
 queue << 'saf' # close后不可追加
 # queue closed (ClosedQueueError)
+
+
+# 限制队列长度可用
+queue = SizedQueue.new(3)
+queue.max # 最大长度
+# => 3
+
